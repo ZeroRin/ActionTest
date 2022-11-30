a1c745638dd3a6c416db5de57a3ce4ebfc7f3b21
ZeroRin <pptxp@qq.com>
2022-11-30 13:49:26 +0800

    test

--- echo.py
+++ echo.py
@@ -1,8 +1,15 @@
 import os
 import subprocess
 import shlex
+from pathlib import Path
 
-# cmd = 'echo "commit-message-tw=blablabla" >> $GITHUB_ENV'
-# print(shlex.split(cmd))
-print(os.environ['GITHUB_ENV'])
-# subprocess.run(cmd,shell=True,env=os.environ)
+class GithubEnv:
+    def __init__(self):
+        self.file = Path(os.environ.get('GITHUB_ENV','env.txt'))
+    def __setitem__(self, name, value):
+        with self.file.open('a') as f:
+            f.write(f'{name}={value}\n')
+
+github_env = GithubEnv()
+for i in range(3):
+    github_env[f"name{i}"] = f'value{i}'
